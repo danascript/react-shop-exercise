08cf8cc3454c109668817d53162655a9fae11d3b
Daniela <daniela.fedyakin@gmail.com>
2018-04-12 15:59:33 +0200

    removed build version

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { render } from "react-dom";
 import "font-awesome/css/font-awesome.css";
+import "./css/style.css";
 
 // importing components
 import App from "./components/App";
